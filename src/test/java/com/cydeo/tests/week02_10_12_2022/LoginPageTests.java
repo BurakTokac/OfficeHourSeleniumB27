0b25c62af6b9136e9c8cd1919a1e8058684007e5
AdamB25 <barsialtunit@gmail.com>
2022-10-12 16:45:06 -0400

    LoginPageTests second test

--- src/test/java/com/cydeo/tests/week02_10_12_2022/LoginPageTests.java
+++ src/test/java/com/cydeo/tests/week02_10_12_2022/LoginPageTests.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.week02_10_12_2022;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,6 +22,12 @@ public class LoginPageTests {
         driver.manage().window().maximize();
         //1-go to home page
         driver.get("https://vytrack.com/");
+
+        //  3- Click Login label
+        String locator = "(//ul[@id='top-menu']/li)[6]";
+        WebElement loginLabel = driver.findElement(By.xpath(locator));
+        wait(1);
+        loginLabel.click();
     }
 
     @AfterMethod
@@ -40,18 +47,36 @@ public class LoginPageTests {
 
     @Test
     public void verify_login_title() {
-        //  3- Click Login label
-        String locator = "(//ul[@id='top-menu']/li)[6]";
-        WebElement loginLabel = driver.findElement(By.xpath(locator));
-        wait(1);
-        loginLabel.click();
-        // 3- Verify the title contains "Login"
-        String expectedTitleContains="Login";
-        String actualTitle = driver.getTitle();
 
+        // 3- Verify the title contains "Login"
+        String expectedTitleContains = "Login";
+        String actualTitle = driver.getTitle();
         Assert.assertTrue(actualTitle.contains(expectedTitleContains));
+    }
+/*
+** TC004 As a user I should be able to login with valid credentials
+
+ 1- Setup the "browser driver"
+ 2- Go to "https://vytrack.com"
+ 3- Click Login label
+ 4- Login to application with username as "User1" and password as "UserUser123"
+ 5- Verify the title contains "Dashboard"
+ */
+
+    @Test
+    public void verify_title_contains_dashboard() {
+        //   4- Login to application with username as "User1" and password as "UserUser123"
+        WebElement username = driver.findElement(By.id("prependedInput"));
+        WebElement password = driver.findElement(By.id("prependedInput2"));
+        username.sendKeys("User1");
+        password.sendKeys("UserUser123" + Keys.ENTER);
+
+        wait(3);
+        //   5- Verify the title contains "Dashboard"
+        Assert.assertTrue(driver.getTitle().contains("Dashboard"));
 
     }
+
 
     public void wait(int seconds) {
         try {
